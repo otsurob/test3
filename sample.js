fbda7855dc573bce6a932ceba758f562ec7b5605
otsurob <tulaon0618@gmail.com>
2023-04-05 15:50:55 +0900

    チェックボックスの練習

---
+++ sample.js
@@ -0,0 +1,33 @@
+function display() {
+    let target = document.getElementById("target").value;
+    const original = target;
+
+    var fruit = document.sampleform.food[0];
+    var drink = document.sampleform.food[1];
+    var main = document.sampleform.food[2];
+    // var answer = document.getElementById("answer");
+    // answer.innerHTML = ""
+
+    var fruit_arr = ["リンゴ", "メロン", "イチゴ"];
+    var drink_arr = ["コーラ", "お茶", "レモンティー"];
+    var main_arr = ["米", "麺", "パン"];
+
+    var result_arr = ["タピオカ"];
+
+
+    if(fruit.checked){
+        result_arr.concat(fruit_arr);
+    }
+
+    if(drink.checked){
+        result_arr.concat(drink_arr);
+    }
+
+    if(main.checked){
+        result_arr.concat(main_arr);
+    }
+
+    document.getElementById("answer") = result_arr;
+    document.getElementById("number") = original
+
+}
